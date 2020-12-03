e080737f47a68159b29b954b751a79aa99ae204a
Grejo Joby <grejo00@gmail.com>
2020-12-04 00:20:36 +0530

    content changes

--- js/main.js
+++ js/main.js
@@ -33,6 +33,37 @@ jQuery(document).ready(function($) {
           });
           
         });
+        $(function() {
+  
+          // Vars
+          var modBtn  = $('#modBtn1'),
+              modal   = $('#modal1'),
+              wrap   = $('#header'),
+              close   = modal.find('.close'),
+              modContent = modal.find('.modal-content');
+          
+          // open modal when click on open modal button 
+          modBtn.on('click', function() {
+            wrap.css('display','none');
+            modal.css('display', 'block');
+            modal.css('z-index', '1000');
+            modContent.removeClass('modal-animated-out').addClass('modal-animated-in');
+            
+          });
+          
+          // close modal when click on close button or somewhere out the modal content 
+          $(document).on('click', function(e) {
+            var target = $(e.target);
+            if(target.is(modal) || target.is(close)) {
+              modContent.removeClass('modal-animated-in').addClass('modal-animated-out').delay(300).queue(function(next) {
+                modal.css('display', 'none');
+                wrap.css('display','');
+                next();
+              });
+            }
+          });
+          
+        });
 
       (function($) {
         $(".accordion > li:eq(0) a")
